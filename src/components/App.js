3753f12eba0d813844cc67bf41e90749b7148ba6
Kush <kushagragarg1998@gmail.com>
2020-01-23 13:39:13 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+
+  render() {
+    return (
+      <div>Kushagra Garg</div>
+    );
+
+   
+  }
+}
+
+export default App; 
